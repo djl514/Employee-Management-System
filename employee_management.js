ddae77575320b8a95128e699aaf4165469f4db2b
David Layne <davidlayne@usf.edu>
2024-10-03 21:54:18 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -19,9 +19,35 @@ Department: ${this.department}`);
 }
 
 const stilgar = new Employee("Stilgar", 2000, "Naib", "Freeman");
+const paul = new Employee("Paul", 3000, "Fedaykin", "Freeman")
+const jessica = new Employee("Jessica", 2000, "Reverend Mother", "Freeman");
 stilgar.getDetails();
 
 
-//2. 
-// const seitch = new Department("Freeman");
-// const arrakeen = new Department("Sardukar");
+//2. Create a Department Class
+
+class Department {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+    getDepartmentSalary(){
+        let totalSalary = 0;
+        this.employees.forEach(employee => {
+            totalSalary += employee.salary;
+        });
+        console.log(`Total Salary for the ${this.name} department: $${totalSalary}`);
+    }
+}
+
+const caladan = new Department("Atreides");
+caladan.addEmployee(paul);
+caladan.addEmployee(stilgar);
+caladan.addEmployee(jessica);
+caladan.getDepartmentSalary();
+
+//3. Create a Manager Class that inherits from Employee
+
