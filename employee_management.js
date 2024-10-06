87ef54b28029cce371acf27339c4818e2be272aa
David Layne <davidlayne@usf.edu>
2024-10-06 14:57:12 -0400

    Handle bonuses in the department salary calculation / Add comments

--- employee_management.js
+++ employee_management.js
@@ -29,28 +29,38 @@ class Department {
     }
     addEmployee(employee){
         this.employees.push(employee);
+        //adds employee to the empty array
     }
     getDepartmentSalary(){
         let totalSalary = 0;
         this.employees.forEach(employee => {
             totalSalary += employee.salary;
+            //uses a loop to add up each individual salary of every employee
         });
         console.log(`Total Salary for the ${this.name} department: $${totalSalary}`);
     }
     
     //4. Handle Bonuses for Managers
     calculateTotalSalaryWithBonus(){
-
+        let totalSalary = 0;
+        this.employees.forEach(employee => {
+            totalSalary += employee.salary;
+            if(employee instanceof Manager){
+                totalSalary += employee.bonus;
+                //similar to getDepartmentSalary, however uses an if statement with instanceof to add the bonus if there is a manager in the array
+            }
+        });
+        console.log(`Total Salary for the ${this.name} department including Manager Bonus: $${totalSalary}`);
     }
 }
 
 //3. Create a Manager Class that inherits from Employee
 
 class Manager extends Employee{
-    constructor(name, salary, position, department, bonus){
-        super(name, salary, position, department);
+    constructor(name, salary, department, bonus){
+        super(name, salary, "Manager", department);
         this.bonus = bonus;
-        this.position = "Manager"; //No matter what position is inputted, position is always set to manager
+        //uses inheritance on the employee class to add a manager to the employees array
     }
     getDetails(){
         console.log(`Employee Information ->
@@ -59,6 +69,7 @@ Salary: $${this.salary}
 Position: ${this.position}
 Department: ${this.department}
 Bonus: $${this.bonus}`);
+//similar getDetails function to employees but overrides it by reestablishing the function
     }
 
 
@@ -68,7 +79,7 @@ Bonus: $${this.bonus}`);
 //5. Create and Manage Departments and Employees // Output
 
 const stilgar = new Employee("Stilgar", 2000, "Naib", "Freman");
-const chani = new Employee("Chani", 3000, "Priestess", "Freman");
+const duncan = new Employee("Duncan", 3000, "Fedaykin", "Freman");
 const jessica = new Employee("Jessica", 2000, "Priestess", "Freman");
 //Creating multiple instances
 
@@ -76,15 +87,21 @@ stilgar.getDetails();
 //Applying the method in the Employee Class to show details
 
 const caladan = new Department("Atreides");
-caladan.addEmployee(chani);
+//Creates an instance of a department
+
+
+caladan.addEmployee(duncan);
 caladan.addEmployee(stilgar);
 caladan.addEmployee(jessica);
 caladan.getDepartmentSalary();
-console.log(caladan);
+//Uses previous employees instated and adds them to the Atreides department
 
-const paul = new Manager("Paul", 5000, "Fedaykin", "Atreides", 1500);
+console.log(caladan);
+//Return the department object
+
+const paul = new Manager("Paul", 5000, "Atreides", 1500);
 paul.getDetails();
 caladan.addEmployee(paul);
 
 
-
+caladan.calculateTotalSalaryWithBonus();
